a177dc7f9315b7acbd7daa76c4c66a6d0f0d0682
naoya shibahara <naoya-shibahara@cookpad.com>
2018-10-05 10:32:15 +0900

    Fix execute test

--- execute_test.rb
+++ execute_test.rb
@@ -4,5 +4,10 @@ Dir.glob('test*.rb').sort.each do |f|
   correct = `ruby #{f}`
   answer = `ruby #{MY_PROGRAM} #{f}`
 
+  result = correct == answer ? 'OK!' : 'NG'
   puts "#{f} => #{correct == answer ? 'OK!' : 'NG'}"
+
+  if result == 'NG'
+    break
+  end
 end
